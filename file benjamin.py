a55ed66cbc3d2001819c3484903e5bae7991036a
Benjamin <benjamin.bresson@protonmail.com>
2020-03-31 23:06:36 +0200

    commit

--- file benjamin.py
+++ file benjamin.py
@@ -1,5 +1,19 @@
 import numpy as np
 import matplotlib as plt
 import numpy.random as rd
-print("Hello World")
-print("Salut")
+
+
+
+
+
+def affiche(M):
+    plt.close()
+    for i in range(len(M)):
+        for j in range(len(M[0])):
+            plt.fill([i,i+1,i+1,i,i],[j,j,j+1,j+1,j],color=C[M[i,j]])
+    plt.xlim(0,n)
+    plt.ylim(0,p)
+    plt.axis('off')
+    plt.show()
+
+
