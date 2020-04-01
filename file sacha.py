2cd32bbc00b53d3c406a7de7d1d858f76d8b1f52
knightaretfact <45062614+knightaretfact@users.noreply.github.com>
2020-04-01 17:07:10 +0200

    update

--- file sacha.py
+++ file sacha.py
@@ -1,2 +1,20 @@
 import numpy
 import pygame
+pygame.init()
+pygame.display.init()
+screen=pygame.display.set_mode((400,400))
+
+cols=10
+lignes=10
+#Array
+def arrayCreate(cols,lignes):
+    a=numpy.zeros((lignes,cols))
+    #print(a)
+    return a
+
+
+#Initialisation
+def setup():
+    arrayCreate(cols,lignes)
+
+
