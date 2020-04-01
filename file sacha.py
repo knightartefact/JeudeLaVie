d3a7a96ef4a2c35239e5e92e8fdcf9e63836eab9
knightaretfact <45062614+knightaretfact@users.noreply.github.com>
2020-04-02 00:46:28 +0200

    update

--- file sacha.py
+++ file sacha.py
@@ -6,15 +6,9 @@ pygame.display.init()
 
 width=400
 height=400
-
 screen=pygame.display.set_mode((width,height))
-
 white=pygame.Color(255,255,255)
-
 resolution=40
-cols=0
-lignes=0
-grille=[]
 
 #Array
 def arrayCreate(cols,lignes):
@@ -22,15 +16,24 @@ def arrayCreate(cols,lignes):
     return tab
 
 def comptageVoisins():
-    print(1)
+    for i in range(lignes):
+        for j in range(cols):
+            
 
 #Initialisation
 def setup():
+    global grille
+    global cols
+    global lignes
     cols= int(width/resolution)
     lignes=int(height/resolution)
-    #Creation de la grille avec des cellules aléatoires (vives ou morts)
     grille=arrayCreate(cols,lignes)
-
+    #Creation de la grille avec des cellules aléatoires (vives ou morts)
+    for i in range(lignes):
+        for j in range(cols):
+            grille[i][j]=random.randint(0,1)
+    draw()
+    
 #update
 
 def draw():
@@ -40,9 +43,10 @@ def draw():
             if event.type == pygame.QUIT:   
                 run=False
                 break
-        screen.fill((0,0,0))
         for i in range(lignes):
             for j in range(cols):
-                grille[i][j]=random.randint(0,1)
                 if grille[i][j]==1:
-                    pygame.draw.rect(screen,white,(j*resolution,i*resolution,resolution-1,resolution-1)
+                    pygame.draw.rect(screen,white,(j*resolution,i*resolution,resolution-1,resolution-1))
+        pygame.display.update()
+
+setup()
