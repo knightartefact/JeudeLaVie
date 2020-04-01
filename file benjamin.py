c51d413a14ad6726c20ec85b66f0750009059075
Benjamin <benjamin.bresson@protonmail.com>
2020-04-01 17:26:38 +0200

    caca

--- file benjamin.py
+++ file benjamin.py
@@ -11,14 +11,14 @@ def terrain(n):
     T=np.zeros((n,n))
 
 
-# Fonction pour placer les cases vivantes au départ
+# Fonction pour placer les cellules vivantes au départ
 
 def cases_vivantes(L):
     for i in range(len(L)):
         T[L[i][0]][L[i][1]]=1
 
 
-# Fonction auxiliaire pour trouver les coordonnées des cases vivantes
+# Fonction auxiliaire pour trouver les coordonnées des cellules vivantes
 
 def vivantes(T):
     global CV
@@ -27,6 +27,22 @@ def vivantes(T):
         for j in range(len(T[0])):
             if T[i][j]==1:
                 CV.append([i,j])
+
+
+''' Fonction auxiliaire pour faire naître une cellule (selon les règles données):
+        Il faut créer une liste auxiliaire qui contient les coordonnées des cellules qui vont naitre
+        ensuite, on utilise l'algo mortCell pour avoir la liste des coordonnées des cellules qui vont
+        mourir. Enfin, on change la liste T avec l'algo iterationT.
+'''
+def naissanceCell(CV):
+    for i in range(len(CV)):
+        s= CV[i,]
+
+
+
+
+
+
 
 def JeuDeLaVie(n,L):
     terrain(n)
